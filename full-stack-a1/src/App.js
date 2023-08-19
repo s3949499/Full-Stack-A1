8b7a2c259605fe27b032f0459bd0b9ae546c58aa
Connor <101156643+s3949499@users.noreply.github.com>
2023-08-19 20:34:22 +1000

    Add files via upload

--- full-stack-a1/src/App.js
+++ full-stack-a1/src/App.js
@@ -1,20 +1,20 @@
-import './App.css';
-import Header from './Pages/Header';
-import Footer from './Pages/Footer';
-import Home from './Pages/Home';
-import SignUp from './Pages/SignUp';
-import SignIn from './Pages/SignIn';
+// import './App.css';
+// import Header from './Pages/Header';
+// import Footer from './Pages/Footer';
+// import Home from './Pages/Home';
+// import SignUp from './Pages/SignUp';
+// import SignIn from './Pages/SignIn';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Home />
-      <SignUp />
-      <SignIn />
-      <Footer />
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header />
+//       <Home />
+//       <SignUp />
+//       <SignIn />
+//       <Footer />
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
