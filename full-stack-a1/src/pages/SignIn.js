54f3da9bc92885c27242205b5858eb7e67223540
“[Matthew <s3822875@student.rmit.edu.au>
2023-08-22 12:37:50 +1000

    sign in, sign up validation and logic

--- full-stack-a1/src/pages/SignIn.js
+++ full-stack-a1/src/pages/SignIn.js
@@ -1,21 +1,51 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function isValidEmail(email) {
+  const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  return regex.test(email);
+}
 
 function SignIn() {
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
     email: '',
     password: ''
   });
+  const [errors, setErrors] = useState({});
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData(prevState => ({ ...prevState, [name]: value }));
   };
 
+  const validateForm = () => {
+    let formErrors = {};
+
+    if (!formData.email || !isValidEmail(formData.email)) formErrors.email = "Invalid email.";
+    if (!formData.password) formErrors.password = "Password is required.";
+
+    setErrors(formErrors);
+    return Object.keys(formErrors).length === 0;
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    const user = JSON.parse(localStorage.getItem('user'));
-    if(user && user.email === formData.email && user.password === formData.password) {
-      // TODO: Handle successful login
+
+    if (validateForm()) {
+      const storedUser = JSON.parse(localStorage.getItem('user'));
+
+      if (storedUser && storedUser.email === formData.email && storedUser.password === formData.password) {
+        alert("Login Successful!");
+
+        // Redirect the user to the home page
+        navigate('/');
+      } else {
+        setErrors({
+          ...errors,
+          general: "Invalid email or password."
+        });
+      }
     }
   };
 
@@ -24,10 +54,17 @@ function SignIn() {
       <h1>Sign In</h1>
       <form onSubmit={handleSubmit}>
         <input type="email" name="email" placeholder="Email" onChange={handleInputChange} required />
+        {errors.email && <span style={{color: 'red'}}>{errors.email}</span>}
+        
         <input type="password" name="password" placeholder="Password" onChange={handleInputChange} required />
+        {errors.password && <span style={{color: 'red'}}>{errors.password}</span>}
+        
+        {errors.general && <span style={{color: 'red', display: 'block'}}>{errors.general}</span>}
+        
         <button type="submit">Sign In</button>
       </form>
     </div>
   );
 }
+
 export default SignIn;
--- full-stack-a1/src/pages/SignUp.js
+++ full-stack-a1/src/pages/SignUp.js
@@ -1,21 +1,68 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function isValidEmail(email) {
+  const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  return regex.test(email);
+}
+
+function validatePassword(password) {
+  const minCharRegex = /^.{8,}$/;
+  const upperCaseRegex = /[A-Z]/;
+  const lowerCaseRegex = /[a-z]/;
+  const digitRegex = /\d/;
+  const specialCharRegex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+
+  const errors = [];
+  if (!minCharRegex.test(password)) errors.push("Minimum 8 characters required.");
+  if (!upperCaseRegex.test(password)) errors.push("At least one uppercase letter required.");
+  if (!lowerCaseRegex.test(password)) errors.push("At least one lowercase letter required.");
+  if (!digitRegex.test(password)) errors.push("At least one number required.");
+  if (!specialCharRegex.test(password)) errors.push("At least one special character required.");
+
+  return errors;
+}
 
 function SignUp() {
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
     name: '',
     email: '',
     password: ''
   });
+  const [errors, setErrors] = useState({});
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData(prevState => ({ ...prevState, [name]: value }));
   };
 
+  const validateForm = () => {
+    let formErrors = {};
+
+    if (!formData.name) formErrors.name = "Name is required.";
+    if (!formData.email) formErrors.email = "Email is required.";
+    else if (!isValidEmail(formData.email)) formErrors.email = "Invalid email format.";
+
+    if (!formData.password) formErrors.password = "Password is required.";
+    else {
+      const passwordValidationErrors = validatePassword(formData.password);
+      if (passwordValidationErrors.length > 0) {
+        formErrors.password = passwordValidationErrors.join(' ');
+      }
+    }
+
+    setErrors(formErrors);
+    return Object.keys(formErrors).length === 0;
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    localStorage.setItem('user', JSON.stringify(formData));
-    // TODO: Handle registration logic
+
+    if (validateForm()) {
+      localStorage.setItem('user', JSON.stringify(formData));
+      navigate('/');
+    }
   };
 
   return (
@@ -23,11 +70,19 @@ function SignUp() {
       <h1>Sign Up</h1>
       <form onSubmit={handleSubmit}>
         <input type="text" name="name" placeholder="Name" onChange={handleInputChange} required />
+        {errors.name && <span style={{color: 'red'}}>{errors.name}</span>}
+        
         <input type="email" name="email" placeholder="Email" onChange={handleInputChange} required />
+        {errors.email && <span style={{color: 'red'}}>{errors.email}</span>}
+        
         <input type="password" name="password" placeholder="Password" onChange={handleInputChange} required />
+        {errors.password && <span style={{color: 'red'}}>{errors.password}</span>}
+        
         <button type="submit">Sign Up</button>
       </form>
     </div>
   );
 }
+
 export default SignUp;
+
