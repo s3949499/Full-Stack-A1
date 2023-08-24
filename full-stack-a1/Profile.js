752db488d85ab1de9a2c8c2eeeffc1525ad058b5
Connor <101156643+s3949499@users.noreply.github.com>
2023-08-24 14:33:37 +1000

    Add files via upload

---
+++ full-stack-a1/Profile.js
@@ -0,0 +1,45 @@
+function Profile() {
+    return (
+      <>
+      <div class= "home-container">
+        <div class="home-bg">
+          <div class="home-content">
+            <h1> Welcome to LOOP WEB </h1>
+            <p>PUT LOOP WEB CONTENT HERE</p>
+            <br></br>
+            <p>PUT LOOP WEB CONTENT HERE</p>
+            <br></br>
+            <p>PUT LOOP WEB CONTENT HERE</p>
+            <br></br>
+            <p>PUT LOOP WEB CONTENT HERE</p>
+            <br></br>
+            <p>PUT LOOP WEB CONTENT HERE</p>
+            <br></br>
+            <p>PUT LOOP WEB CONTENT HERE</p>
+            <br></br>
+            <p>PUT LOOP WEB CONTENT HERE</p>
+            <br></br>
+            <p>PUT LOOP WEB CONTENT HERE</p>
+            <br></br>
+            <p>PUT LOOP WEB CONTENT HERE</p>
+            <br></br>
+            <p>PUT LOOP WEB CONTENT HERE</p>
+            <br></br>
+            <p>PUT LOOP WEB CONTENT HERE</p>
+            <br></br>
+            <p>PUT LOOP WEB CONTENT HERE</p>
+            <br></br>
+            <p>PUT LOOP WEB CONTENT HERE</p>
+            <br></br>
+            <p>PUT LOOP WEB CONTENT HERE</p>
+            <br></br>
+            <p>PUT LOOP WEB CONTENT HERE</p>
+            <br></br>
+            </div>
+          </div>
+          </div>
+      </>
+    );
+  }
+  export default Profile;
+  
