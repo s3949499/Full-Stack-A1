6eff62dfca8f3e127fcc2e01c90201e7426858ad
Connor <101156643+s3949499@users.noreply.github.com>
2023-08-24 21:24:58 +1000

    Landing page w/Movies
    I forgor 💀

--- full-stack-a1/src/pages/Home.js
+++ full-stack-a1/src/pages/Home.js
@@ -3,7 +3,7 @@ function Home() {
     <>
       <div className="home-container">
         <div className="img-container">
-          <img className="cinema-BG" src='cinemaBG2.jpg'></img>
+          <img className="cinema-BG" src='cinemaBG2.jpg' alt="cinema backdrop"></img>
           <div className="overlay">
             <div className="ov-text">
               <h1> Begin your cinema experience today</h1>
@@ -11,26 +11,92 @@ function Home() {
           </div>
 
         </div>
+        {/* Loop Cinemas information  */}
         <div className="home-content">
           <div className="home-s1">
-            <img className="icon" src='cinema.png'></img>
+            <img className="icon" src='cinema.png' alt="cinema icon"></img>
             <h2> What is Loop Cinemas?</h2>
             <p> Loop Cinemas is local, long running cinema operator with several cinema locations across all of Australia. </p>
           </div>
 
           <div className="home-s2">
-            <img className="icon" src='goals.png'></img>
+            <img className="icon" src='goals.png' alt="goals icon"></img>
             <h2> Our Goals: </h2>
             <p> Our goal at Loop Cinemas is to provide Australians with premium unique experiences whilst including the community in our cinema experiences. </p>
           </div>
 
           <div className="home-s3">
-            <img className="icon" src='activity.png'></img>
+            <img className="icon" src='activity.png' alt="activities icon"></img>
             <h2> Activities we provide: </h2>
-            <p>Loop Cinemas hold a variety of community events, art shows in addition to the lattest and greatest film releases.</p>
+            <p>Loop Cinemas hold a variety of community events, art shows in addition to the latest and greatest film releases.</p>
           </div>
-        </div >
-      </div >
+          </div>
+  {/* movie info */}
+  <div className="movie-section">
+          <h1 className="movie-h1"> Featured movies</h1>
+          <div className="movies">
+                <div className="movie-1">
+                  <img className="coverArt" src='lucas.jpg' alt="movie cover"></img>
+                  <h2>Movie 1: Showing Times</h2>
+                  <h3>-- 20/07/2023 --</h3>
+                  <p>12:30-14:30</p>
+                  <p>17:30-19:30</p>
+                  <h3>-- 21/07/2023 --</h3>
+                  <p>11:00-13:00</p>
+                  <p>16:00-18:00</p>
+                  
+                </div>
+                <div className="movie-2">
+                  <img className="coverArt" src='tim.jpg' alt="movie cover"></img>
+                  <h2>Movie 2: Showing Times</h2>
+                  <h3>-- 20/07/2023 --</h3>
+                  <p>15:00-17:00</p>
+                  <p>20:00-22:00</p>
+                  <h3>-- 21/07/2023 --</h3>
+                  <p>13:30-15:30</p>
+                  <p>18:30-20:30</p>
+
+                </div>
+                <div className="movie-3">
+                  <img className="coverArt" src='stormseeker.jpg' alt="movie cover"></img>
+                  <h2>Movie 3: Showing Times</h2>
+                  <h3>-- 20/07/2023 --</h3>
+                  <p>10:00-12:00</p>
+                  <p>14:00-16:00</p>
+                  <h3>-- 21/07/2023 --</h3>
+                  <p>12:00-14:00</p>
+                  <p>17:00-19:00</p>
+                </div>
+                <div className="movie-4">
+                  <img className="coverArt" src='diego.jpg' alt="movie cover"></img>
+                  <h2>Movie 4: Showing Times</h2>
+                  <h3>-- 20/07/2023 --</h3>
+                  <p>16:30-18:30</p>
+                  <p>19:30-21:30</p>
+                  <h3>-- 21/07/2023 --</h3>
+                  <p>14:30-16:30</p>
+                  <p>19:30-21:30</p>
+                </div>
+                <div className="movie-5">
+                  <img className="coverArt" src='laura.jpg' alt="movie cover"></img>
+                  <h2>Movie 5: Showing Times</h2>
+                  <h3>-- 20/07/2023 --</h3>
+                  <p>13:00-15:00</p>
+                  <p>18:00-20:00</p>
+                  <h3>-- 21/07/2023 --</h3>
+                  <p>15:00-17:00</p>
+                  <p>20:00-22:00</p>
+                </div>
+              </div>
+        </div>
+    </div>
+
+              
+   
+
+
+
+      
     </>
   );
 }
---
+++ full-stack-a1/src/pages/Nav.js
@@ -0,0 +1,38 @@
+// Holds all page common attributes: Header, navbar, footer
+
+import { Outlet, Link, } from "react-router-dom";
+
+function Common(){
+  return (
+    <>
+      <div class = "header-container">
+       <header>
+        <img class = "header-logo" src="tv.png" alt="Logo"/>
+        <h1 class = "h1-header"> LOOP WEB </h1>
+       </header>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link className="navbar-link" to="/">HOME</Link>
+          </li>
+          <li><Link className="navbar-link" to="/SignIn">SIGN IN</Link></li>
+          <li>
+            <Link className="navbar-link" to="/SignUp">SIGN UP</Link>
+          </li>
+       
+        </ul>
+      </nav>
+
+      <Outlet />
+      <div>
+       <footer>
+        <h1 class = "h1-footer"> Loop Web </h1>
+        <p class = "p-footer"> Footer </p>
+       </footer>
+      </div>
+    </>
+  )
+};
+
+export default Common;
