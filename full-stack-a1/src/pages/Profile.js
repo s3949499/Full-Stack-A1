dcee4f8abd85c9ef2b741149c94d6da704650546
Connor <101156643+s3949499@users.noreply.github.com>
2023-08-25 16:59:43 +1000

    profile (no delete or edit functions yet)

--- full-stack-a1/src/pages/Profile.js
+++ full-stack-a1/src/pages/Profile.js
@@ -1,9 +1,23 @@
+import React from 'react';
+
 function Profile() {
-    return (
-      <>
-        <h1>Profile</h1>
-      </>
-    );
-  }
-  export default Profile;
-  
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  return (
+    <div className='profile-Container'>
+
+
+    <div className='profile-details'>
+    <h2>Profile</h2>
+    <div className='userName'>
+    <img className="profile-Icon" src="profileIcon.png" alt="displayed next to username"></img>
+    <p className='userName-text'> {user.name}</p>
+    </div>
+    <p> {user.email}</p>
+    </div>
+
+  </div>
+  );
+}
+
+export default Profile;
